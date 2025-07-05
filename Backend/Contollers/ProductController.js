7d9a1159775cc4400dd0be7d391aae7723054fdc
amithabhghosh <amithabhghosh@gmail.com>
2025-07-05 11:40:57 +0530

    Fish Edit and Delete EndPoint Created

--- Backend/Contollers/ProductController.js
+++ Backend/Contollers/ProductController.js
@@ -42,18 +42,10 @@ const getFish = async (req,res)=>{
 
 const deleteFish = async (req,res)=>{
   try {
-        const ownerId = req.user.id
+        const owner = req.user.id
         const { fishId } = req.params;
-const updatedOwner = await Owner.findByIdAndUpdate(ownerId,
-    {
-        $pull:{inventory : {_id : fishId}}
-    },
-    {new:true}
-)
-if(!updatedOwner){
-    return res.status(400).json({success:false,message:"Owner Not Found"})
-}
-res.status(201).json({success:true,message:"Fish Deleted SuccessFully",inventory:updatedOwner.inventory})
+await Fish.deleteOne({_id:fishId,owner})
+res.status(201).json({success:true,message:"Fish Deleted SuccessFull"})
     } catch (error) {
         res.status(500).json({success:false,message:error.message})
     }
@@ -61,44 +53,26 @@ res.status(201).json({success:true,message:"Fish Deleted SuccessFully",inventory
 
 const editFish = async (req,res)=>{
         try {
-        const {fishName,price,quantity} = req.body
-        const ownerId = req.user.id
+        const {availableQuantityKg} = req.body
+        const owner = req.user.id
         const {fishId} = req.params
-        const updatedOwner = await Owner.findOneAndUpdate(
-            {_id:ownerId,"inventory._id":fishId},
-            {
-$set:{
-    "inventory.$.fishName":fishName,
-    "inventory.$.price":price,
-    "inventory.$.quantity":quantity
-}
-
-            },
-            {new:true}
-        )
-if(!updatedOwner){
-    return res.status(400).json({success:false,message:"Item Not Found"})
-}
-res.status(201).json({success:true,message:"Updated SuccessFully",inventory:updatedOwner.inventory})
+       
+       
+        const updatedFish = await Fish.findByIdAndUpdate({_id:fishId},{
+            availableQuantityKg},{new:true})
+            if(!updatedFish){
+              return  res.status(400).json({success:false,message:"Fish Not Found"})
+            }
+            return res.status(201).json({success:true,message:"Fish Quantity Updated"})
     } catch (error) {
         res.status(500).json({success:false,message:error.message})
     }
 }
 
-const getAllFish = async (req, res) => {
-  try {
-    const allOwners = await Owner.find().select('inventory');
 
-    const allFishes = allOwners.flatMap(owner => owner.inventory);
-
-    res.status(200).json({ success: true, fishes: allFishes });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
 
 const getFishByPincode = async (req,res)=>{
    
 }
 
-module.exports = {addFish,getFish,deleteFish,editFish,getAllFish,getFishByPincode}
+module.exports = {addFish,getFish,deleteFish,editFish,getFishByPincode}
